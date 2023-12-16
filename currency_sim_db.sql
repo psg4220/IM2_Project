bea05f4201eea81a062609b1aaaae2171dbc886e
psg4220 <psg4220beats@proton.me>
2023-12-16 22:11:15 +0800

    Changes in currency_sim_db.sql

--- currency_sim_db.sql
+++ currency_sim_db.sql
@@ -74,6 +74,8 @@ FROM username;
 
 DELIMITER //
 
+-- Procedures;
+
 CREATE PROCEDURE transfer_funds(
     IN p_sender_account_id INT,
     IN p_sender_password VARCHAR(250),
@@ -84,6 +86,7 @@ CREATE PROCEDURE transfer_funds(
 BEGIN
     DECLARE sender_balance DECIMAL(12,2);
     DECLARE receiver_balance DECIMAL(12,2);
+    DECLARE receiver_account_count INT;
 
     -- Check if sender account exists and password is correct
     SELECT balance INTO sender_balance
@@ -95,6 +98,19 @@ BEGIN
     SELECT balance INTO receiver_balance
     FROM currency_balance
     WHERE account_id = p_receiver_account_id AND currency_id = p_currency_id;
+
+    -- Check if reciever account exists
+    SELECT COUNT(*) INTO receiver_account_count
+    FROM username
+    WHERE id = p_receiver_account_id AND p_receiver_account_id != 0;
+
+    IF receiver_account_count = 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Receiver account doesnt exist';
+    END IF;
+
+    IF p_amount <= 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Amount must be greater than zero';
+    END IF;
 
     IF sender_balance IS NOT NULL AND sender_balance >= p_amount THEN
         -- Update sender's balance
@@ -123,14 +139,15 @@ BEGIN
             (SELECT id FROM currency_balance WHERE account_id = p_sender_account_id AND currency_id = p_currency_id),
             COALESCE((SELECT id FROM currency_balance WHERE account_id = p_receiver_account_id AND currency_id = p_currency_id), LAST_INSERT_ID()),
             p_amount,
-            NOW()
+            NOW() -- You can use the current timestamp function of your DBMS
         );
 
         SELECT 'Transaction successful' AS status;
     ELSE
-        SELECT 'Transaction failed. Insufficient funds, invalid accounts, or incorrect password' AS status;
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Transaction failed. Either insufficient Balance or Invalid sender ID';
     END IF;
-END 
+END
 
 CREATE PROCEDURE `create_currency`(
     IN in_currency_name VARCHAR(250),
@@ -141,13 +158,13 @@ CREATE PROCEDURE `create_currency`(
 BEGIN
     -- Check if the currency with the given name already exists
     DECLARE currency_count INT;
-    
+
     IF NOT EXISTS (SELECT 1 FROM username WHERE id = in_master_currency_id AND pass = in_master_currency_pass) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Account doesnt exist';
     END IF;
-    
+
     SELECT COUNT(*) INTO currency_count FROM currency WHERE name = in_currency_name;
-    
+
     IF currency_count > 0 THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Currency with this name already exists';
     END IF;
@@ -155,13 +172,13 @@ BEGIN
     -- Insert new currency
     INSERT INTO currency(name, master_currency_balance_id)
     VALUES (in_currency_name, in_master_currency_id);
-    
+
     INSERT INTO currency_balance(account_id, currency_id, balance)
     VALUES (in_master_currency_id, LAST_INSERT_ID(), in_max_supply);
     SET @last_currency_id := LAST_INSERT_ID();
     INSERT INTO transaction_record(sender_currency_balance_id, receiver_currency_balance_id, amount, time_created)
     VALUES (@last_currency_id, @last_currency_id, in_max_supply, NOW());
-    
+
 END
 
 CREATE PROCEDURE `create_account`(
@@ -173,10 +190,10 @@ BEGIN
     IF EXISTS(SELECT 1 FROM username WHERE username = p_username) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Account already exist';
     END IF;
-    
+
     INSERT INTO username(username, pass)
     VALUES (p_username, p_pass);
-    
+
 END
 
 CREATE PROCEDURE retrieve_bank_statement(
@@ -188,9 +205,19 @@ BEGIN
     IF NOT EXISTS (SELECT 1 FROM username WHERE username = p_username AND pass = p_password) THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Account doesnt exist';
     END IF;
-    
+
     SELECT receiver_account, currency_name, amount, time_created FROM transaction_record_view WHERE sender_account = p_username AND currency_name = p_currency_name;
 END
+
+CREATE PROCEDURE delete_account(
+    IN p_username VARCHAR(250),
+    IN p_password VARCHAR(250)
+)
+BEGIN
+    DELETE FROM username WHERE username = p_username AND pass = p_password;
+END
+
+-- Triggers;
 
 CREATE TRIGGER is_same_sender_receiver
 BEFORE INSERT
@@ -198,14 +225,14 @@ ON transaction_record
 FOR EACH ROW
 BEGIN
     DECLARE source_number INT;
-    
+
     -- Count the number of records with the same sender and receiver currency balance IDs
     SELECT COUNT(*) INTO source_number
     FROM transaction_record
     WHERE sender_currency_balance_id = NEW.sender_currency_balance_id
     AND receiver_currency_balance_id = NEW.receiver_currency_balance_id
     AND NEW.sender_currency_balance_id = NEW.receiver_currency_balance_id;
-    
+
     -- Check if there is already a record with the same sender and receiver
     IF source_number = 1 THEN
         SIGNAL SQLSTATE '45000'
@@ -225,5 +252,4 @@ END
 //
 
 DELIMITER ;
-
 
